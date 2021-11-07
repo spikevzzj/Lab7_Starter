9128129f7b9f478ccd46fa9661bd7131eb8c6025
Zijian Zhang <91357781+spikevzzj@users.noreply.github.com>
2021-11-06 19:56:16 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,12 +3,27 @@
 
 const CACHE_NAME = 'lab-7-starter';
 
+var urlsToCache = [
+  '.',
+  'assets/styles/main.css',
+  'assets/scripts/main.js',
+  'assets/scripts/Router.js'
+];
+
 // Once the service worker has been installed, feed it some initial URLs to cache
 self.addEventListener('install', function (event) {
   /**
    * TODO - Part 2 Step 2
    * Create a function as outlined above
    */
+
+   event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
 });
 
 /**
@@ -21,6 +36,8 @@ self.addEventListener('activate', function (event) {
    * TODO - Part 2 Step 3
    * Create a function as outlined above, it should be one line
    */
+
+   event.waitUntil(clients.claim());
 });
 
 // Intercept fetch requests and store them in the cache
@@ -29,4 +46,16 @@ self.addEventListener('fetch', function (event) {
    * TODO - Part 2 Step 4
    * Create a function as outlined above
    */
+
+   event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
 });
